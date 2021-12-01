8b5d6da08833558c6cc13d1ec5812876da75db84
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-12-01 16:32:55 +0900

    today's commit

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "../styles/layouts.css";
 
 export default function MainContainer() {
   return <main></main>;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { MainContainer } from "../containers";
+import "../styles/layouts.css";
 
 export default function Router() {
   return (
-    <div className="Router">
+    <div className="page">
       <Routes>
         <Route path="/" component={MainContainer} />
       </Routes>
