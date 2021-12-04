3cd63f197a54a1f5ac5949a366227f9910230402
sung-ilkim <skim88.1942@gmail.com>
2021-12-04 16:45:49 +0900

    create nav menu in header

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,5 +2,11 @@ import * as React from "react";
 import "../styles/layouts.css";
 
 export default function Header() {
-  return <header></header>;
+  return (<header>
+    <nav>
+      <ul>
+        <li><a href="/">home</a></li>
+      </ul>
+    </nav>
+  </header>);
 }
