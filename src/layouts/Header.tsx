12ff5fbc60c61ffe5b8483aed7a41b08c017e3c2
sung-ilkim <skim88.1942@gmail.com>
2021-12-05 16:08:07 +0900

    set header layouts

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -2,11 +2,19 @@ import * as React from "react";
 import "../styles/layouts.css";
 
 export default function Header() {
-  return (<header>
+  return (
+  <header>
+    <div className="logo">
+
+    </div>
+    <div className="line" />  
     <nav>
       <ul>
-        <li><a href="/">home</a></li>
+        <li className="header_nav first_child"><a href="/">home</a></li>
+        <li><a href="#">2222</a></li>
+        <li><a href="#">33333333333</a></li>
       </ul>
     </nav>
+    <div className="line" />
   </header>);
 }
